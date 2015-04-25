d4c43d3d5f71da5243fcc9f6fb894f183b1186c1
sefvef <alessio.scalici@gmail.com>
2015-04-25 22:14:20 +0100

    bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 var inject = require('gulp-inject');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
@@ -9,7 +9,7 @@ var less = require('gulp-less');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename2');
 var del = require('del');
-
+var path = require('path');
 
 var mode = 'dev';
 var templateMode = 'js';
@@ -26,13 +26,71 @@ gulp.task('moveFiles', ['clean'], function(){
 });
 
 
-gulp.task('inject', ['moveFiles'], function () {
+gulp.task('inject', ['moveFiles', 'less'], function () {
     var target = gulp.src('./app/index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths:
     var sources = gulp.src(['./build/app.js', './build/feat/**/*.js', './build/feat/**/*.css'], {read: false});
 
     return target.pipe(inject(sources))
         .pipe(gulp.dest('./build'));
+});
+
+gulp.task('less', function () {
+    return gulp.src([
+        './app/bower_components/bootstrap/less/variables.less',
+        './app/bower_components/bootstrap/less/mixins.less',
+
+       './app/bower_components/bootstrap/less/normalize.less',
+        './app/bower_components/bootstrap/less/print.less',
+        //'./app/bower_components/bootstrap/less/glyphicons.less',
+
+     //   './app/bower_components/bootstrap/less/scaffolding.less',
+     //   './app/bower_components/bootstrap/less/type.less',
+        './app/bower_components/bootstrap/less/code.less',
+        './app/bower_components/bootstrap/less/grid.less',
+        './app/bower_components/bootstrap/less/tables.less',
+        './app/bower_components/bootstrap/less/forms.less',
+        './app/bower_components/bootstrap/less/buttons.less',
+
+        // Components
+        /*       "./app/bower_components/bootstrap/less/component-animations.less",
+        "./app/bower_components/bootstrap/less/dropdowns.less",
+        "./app/bower_components/bootstrap/less/button-groups.less",
+        "./app/bower_components/bootstrap/less/input-groups.less",
+        "./app/bower_components/bootstrap/less/navs.less",
+        "./app/bower_components/bootstrap/less/navbar.less",
+        "./app/bower_components/bootstrap/less/breadcrumbs.less",
+        "./app/bower_components/bootstrap/less/pagination.less",
+        "./app/bower_components/bootstrap/less/pager.less",
+        "./app/bower_components/bootstrap/less/labels.less",
+        "./app/bower_components/bootstrap/less/badges.less",
+        "./app/bower_components/bootstrap/less/jumbotron.less",
+        "./app/bower_components/bootstrap/less/thumbnails.less",
+        "./app/bower_components/bootstrap/less/alerts.less",
+        "./app/bower_components/bootstrap/less/progress-bars.less",
+        "./app/bower_components/bootstrap/less/media.less",
+        "./app/bower_components/bootstrap/less/list-group.less",
+        "./app/bower_components/bootstrap/less/panels.less",
+        "./app/bower_components/bootstrap/less/responsive-embed.less",
+        "./app/bower_components/bootstrap/less/wells.less",
+        "./app/bower_components/bootstrap/less/close.less",
+
+        // Components w/ JavaScript
+        "./app/bower_components/bootstrap/less/modals.less",
+        "./app/bower_components/bootstrap/less/tooltip.less",
+        "./app/bower_components/bootstrap/less/popovers.less",
+        "./app/bower_components/bootstrap/less/carousel.less",
+
+        // Utility classes
+        "./app/bower_components/bootstrap/less/utilities.less",
+        "./app/bower_components/bootstrap/less/responsive-utilities.less"
+  */      ])
+        .pipe(less({
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(concat('bootstrap.css'))
+        .pipe(minCss())
+        .pipe(gulp.dest('./build/css'));
 });
 
 
@@ -53,7 +111,7 @@ gulp.task('js', ['clean'], function () {
 });
 
 
-gulp.task('pInject', ['vendor', 'js', 'pTemplates'], function () {
+gulp.task('pInject', ['vendor', 'js', 'pTemplates', 'less'], function () {
     var target = gulp.src('./app/index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths:
     var sources = gulp.src(['./build/js/*.js', './build/css/*.css'], {read: false});
@@ -104,20 +162,33 @@ gulp.task('pMinifyIndex', ['pInject'], function () {
 
 gulp.task('prod', ['vendor', 'js', 'pInject', 'pTemplates', 'pMinifyIndex']);
 
-gulp.task('prod-js', function () {
-    templateMode = 'js';
-    return gulp.run('prod');
-});
-
-gulp.task('prod-ajax', function () {
-    templateMode = 'ajax';
-    return gulp.run('prod');
-});
-
 
 gulp.task('dev', function () {
     mode = 'dev';
     return gulp.run('inject');
 });
 
+gulp.task('default', function (mod, templ, help) {
+
+    console.log(mod, templ, help);
+    if (help || (mod && mod !== 'prod' && mod !== 'dev') || (templ && templ !== 'ajax' && templ !== 'js')) {
+
+        console.log("Usage: gulp [options]" +
+        "\n  Options:" +
+        "\n    -m { dev | prod } (default: dev)" +
+        "\n       dev  : development build, no optimization" +
+        "\n       prod : production build, with optimizations like minification and obfuscation" +
+        "\n    -t { ajax | js } (default: ajax)" +
+        "\n       ajax : the templates are just copied in the build directory and requested as needed" +
+        "\n       js   : the templates are injected in the angular templateCache and served as one js file" +
+        "");
+        return;
+    }
+
+    templateMode = templ || 'ajax';
+    mode = mod || 'dev';
+
+
+    return gulp.run(mode);
+});
 
